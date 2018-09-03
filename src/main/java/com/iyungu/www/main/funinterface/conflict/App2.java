5a981ccc05487bdf8e108a80fb3376ede931c600
John_Rui <3254318771@qq.com>
2018-09-03 08:41:02 +0800

    *：接口函数

---
+++ src/main/java/com/iyungu/www/main/funinterface/conflict/App2.java
@@ -0,0 +1,14 @@
+package com.iyungu.www.main.funinterface.conflict;
+
+/**
+ * @ProjectName: java8Demo
+ * @Author： @iyungu.com
+ * @Description:
+ * @Date 2018/9/1  18:57
+ * @Modified By:
+ */
+public class App2 implements A,B,C {
+    public static void main(String[] args) {
+        new App2().sayHello();
+    }
+}
---
+++ src/main/java/com/iyungu/www/main/funinterface/conflict/App3.java
@@ -0,0 +1,19 @@
+package com.iyungu.www.main.funinterface.conflict;
+
+/**
+ * @ProjectName: java8Demo
+ * @Author： @iyungu.com
+ * @Description:
+ * @Date 2018/9/1  18:59
+ * @Modified By:
+ */
+public class App3 implements A,B {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello this is APP3");
+    }
+
+    public static void main(String[] args) {
+        new App3().sayHello();
+    }
+}
